f8a3b5e6014c6070c106239280a70932bdef5021
Argroth <basic15pl@gmail.com>
2019-03-26 13:16:33 +0100

    Working controllers (without connection to passport and react)

--- controller/project-controller.js
+++ controller/project-controller.js
@@ -1,41 +1,68 @@
 const Project = require('../model/project-model');
 
-let d = new Date();
-
 exports.index = (req, res) => {
-     Project.find({}, (err, projectsSelected) => {
-        return res.json({projectsSelected});
+     Project.find({}, (err, projectSelected) => {
+        return res.json({projectSelected});
      })
 };
 
 exports.show = (req, res) => {
-     return res.json({id: req.params.project_id});
+    Project.findById({_id: req.params.project_id}, (err, projectSelected) => {
+       if(err) throw err;
+       return res.json({projectSelected});
+    });
 };
 
+exports.edit = (req, res) => {
+    Project.findById({_id: req.params.project_id}, (err, projectSelected) => {
+        if(err) throw err;
+        return res.json({projectSelected});
+    })
+};
+
+//TODO Add owner from passport
+//TODO Add data to gantt chart
 exports.create = (req, res) => {
      var project = new Project({
-         name: '123',
-         description: '12333',
+         name: req.body.name,
+         description: req.body.description,
+         owner: 'Łukasz Gronczakiewicz',
+         tags: [req.body.tags],
          meta: {
-             createdAt: d.getDate(),
-             updatedAt: d.getDate(),
-             createdBy: 'lukasz@wp.pl',
-             updatedBy: 'lukasz@wp.pl'
+             createdAt: Date.now(),
+             updatedAt: Date.now(),
+             createdBy: 'Łukasz Gronczakiewicz',
+             updatedBy: 'Łukasz Gronczakiewicz'
          },
          ganttChart: {
              x: 22,
              y: 33
          }
-     })
+     });
 
-    project.save((err, projects) => {
+    project.save((err, projectDetails) => {
        if(err) throw err;
-       res.send('zapisano!' + projects);
+       res.send('Saved: ' + projectDetails);
     });
 };
 
+//TODO updatedBy fetch from passport user
 exports.update = (req, res) => {
-    res.send('project update');
+    Project.findByIdAndUpdate({_id: req.params.project_id}, (err, projectSelected) => {
+        if(err) throw err;
+
+        projectSelected.name = req.body.name;
+        projectSelected.description = req.body.description;
+        projectSelected.tags = req.body.tags;
+        projectSelected.meta.updatedAt = Date.now();
+        projectSelected.meta.updatedBy = "Łukasz Gronczakiewicz";
+
+        projectSelected.save((err, projectDetails) => {
+           if(err) throw err;
+           res.send('Updated: ' + projectDetails);
+        });
+
+    });
 };
 
 
@@ -43,8 +70,7 @@ exports.update = (req, res) => {
 //TODO get id from react frontend
 //TODO Protection!
 exports.delete = (req, res) => {
-    Project.findByIdAndRemove({_id: req.params.project_id}, (err, projectSelected => {
-
+    Project.findByIdAndRemove({_id: req.params.project_id}, (err, projectSelected) => {
     const log = {
         message: 'Project deleted: ',
         project_id: projectSelected._id
@@ -52,8 +78,7 @@ exports.delete = (req, res) => {
 
     console.log(log);
     res.redirect('/projects')
-
-    }));
+    });
 };
 
 
--- routes/project-routes.js
+++ routes/project-routes.js
@@ -4,7 +4,8 @@ module.exports= (app) => {
 
      app.get('/projects', projectController.index);
      app.get('/project/:project_id', projectController.show);
-     app.post('/project', projectController.create);
+     app.get('/project', projectController.create);
+     app.get('/project/edit/:project_id', projectController.edit);
      app.post('/project/update', projectController.update);
      app.get('/project/delete/:project_id', projectController.delete);
 
